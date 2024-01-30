943788ca1fc878994f04417635101d67eaf442fd
Kauan Sena <lkrs@protonmail.com>
2024-01-30 11:28:47 -0300

    Create termux-setup.sh

---
+++ termux-setup.sh
@@ -0,0 +1,12 @@
+termux-change-repo
+pkg install termux-am
+termux-setup-storage
+apt update
+apt upgrade -y
+apt install git python3 nodejs neofetch
+pkg install rust
+apt-get install binutils
+clear
+neofetch
+echo "Setup finished, run bash"
+echo " "
